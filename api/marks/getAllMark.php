0ab9b4d9e5aedde754ab0d0ba5db3963ec7a08e2
beshe <besherhamze97@gmail.com>
2024-08-09 00:32:49 +0300

    edit in getAllMark API (modify query to get mark)

--- api/marks/getAllMark.php
+++ api/marks/getAllMark.php
@@ -10,8 +10,15 @@ include "../../lib/functions.php" ;
 $id_student = $input['id_student'];
 
 
-$stmt = $con ->prepare("SELECT * FROM `marks` WHERE id_student = '$id_student'");
-$stmt ->execute() ;
+$stmt = $con->prepare("
+    SELECT m.mark, sub.name AS subject_name, sp.name AS supervisor_name
+    FROM marks m
+    INNER JOIN subject sub ON m.id_subject = sub.id
+    INNER JOIN supervisors sp ON m.id_supervisor = sp.id
+    WHERE m.id_student = $id_student
+");
+
+$stmt->execute();
 $count  = $stmt -> rowCount() ;
 $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
